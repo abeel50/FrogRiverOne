c612579616dd3517940bd8d58289fb4972d1047a
abeel50 <abeelmuhammad50@gmail.com>
2022-06-15 18:44:43 +0500

    working...

---
+++ FrogRiverOne.py
@@ -0,0 +1,12 @@
+def findLeaves(X,A):
+    if len(A) > 0:
+        p = A.find(X)
+        temp_A = A[:p+1]
+        cmp = [0] * X
+        for i in temp_A:
+            
+
+
+def solution(X, A):
+    # write your code in Python 3.6
+    return findLeaves(X,A)
