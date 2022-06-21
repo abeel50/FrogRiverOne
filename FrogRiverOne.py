e1fc8fa122e800fe3790c5ac716efbc04a428e47
abeel50 <abeelmuhammad50@gmail.com>
2022-06-21 21:08:42 +0500

    27

--- FrogRiverOne.py
+++ FrogRiverOne.py
@@ -1,12 +1,12 @@
-def findLeaves(X,A):
-    if len(A) > 0:
-        p = A.find(X)
-        temp_A = A[:p+1]
-        cmp = [0] * X
-        for i in temp_A:
-            
-
+def frogJumps(X, A , I):
+    if I >= 0 and I < len(A) and X in A[I:]:
+        first = A.index(X,I)
+        aSum , xSum = sum(list(set(A[0:first+1]))) , (X*(X+1))/2
+        return first if aSum == xSum else frogJumps(X, A, I+1)
+    else:
+        return -1
 
 def solution(X, A):
     # write your code in Python 3.6
-    return findLeaves(X,A)
+    return frogJumps(X,A,0)
+print(solution(5, [1, 2, 3, 5, 3, 1]))
